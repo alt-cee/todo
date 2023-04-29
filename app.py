bcbc6a731940dfaad4c000192aa1249b07744d16
ai-lien <a.trancong@gmail.com>
2023-04-29 15:35:12 -0700

    add sqlite database

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+db.init_app(app)
+
+class todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
 
 @app.route("/")
 def index():
+    print(todo.query.all())
     return render_template("base.html")
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all() 
+        
     app.run(debug=True)
+    
